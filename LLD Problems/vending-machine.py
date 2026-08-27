11539e134855b09868a9607759c6e93a7673f60a
Shobhit Rajawat <rajawatshobhit4@gmail.com>
2026-08-27 14:31:31 +0530

    Refactor Vending Machine for state management
    Refactor vending machine code to improve state management and thread safety. Update product selection, money insertion, and signal processing methods.

--- LLD Problems/vending-machine.py
+++ LLD Problems/vending-machine.py
@@ -10,7 +10,31 @@
 # these steps are basically different states that a vending machine will be at a particular instance -> state pattern ?
 # all these steps will be a different class
 
+# Vending Machine
+
+# Requirements
+# The vending machine should support multiple products with different prices and quantities.
+# The machine should accept coins and notes of different denominations.
+# The machine should dispense the selected product and return change if necessary.
+# The machine should keep track of the available products and their quantities.
+# The machine should handle multiple transactions concurrently and ensure data consistency.
+# The machine should provide an interface for restocking products and collecting money.
+# The machine should handle exceptional scenarios, such as insufficient funds or out-of-stock products.
+
+
+# entities: Vending_Machine, Products, Coins, Inventory
+
 from abc import ABC,abstractmethod
+from enum import Enum
+import threading
+
+class Denomination(Enum):
+    COIN_1=1
+    COIN_2=2
+    COIN_5=5
+    COIN_10=10
+    NOTE_50=50
+    NOTE_100=100
 
 class Product:
     def __init__(self,name,price,quantity):
@@ -18,100 +42,114 @@ class Product:
         self.price=price
         self.quantity=quantity
     def __repr__(self):
-        return f"{self.name} (₹{self.price}, qty={self.quantity})"
+        return f"{self.name}"
 
-    
-# this base class State basically contains all the methods that will be used by different states
 class State(ABC):
-    """
-    Base State. Every concrete state overrides only the
-    method(s) relevant to it. The rest fall back to this
-    default "not allowed" behavior.
-    """
     def select_product(self,machine,code):
-        print("cannot select a product right now")
-    def insert_money(self,machine,amount):
-        print("cannot insert money right now")
+        print("Cannot select a product")
+    def insert_money(self,machine,denomination):
+        print("Cannot insert amount")
     def process_signal(self,machine):
-        print('cannot process dispensing signal right now')
+        print("Cannot process signal")
     def dispense(self,machine):
-        print('cannot dispense right now')
+        print("cannot dispense right now")
     def deliver(self,machine):
-        print('cannot deliver right now')
+        print("Cannot deliver right now")
 
 class VendingMachine:
     def __init__(self):
-        self.inventory={
-            "A1": Product("Coke", 25, 5),
-            "A2": Product("Chips", 20, 3),
-            "A3": Product("Water", 15, 0)
+        self._inventory={
+            "A1":Product("Coke",25,5),
+            "A2":Product("Chips",20,3),
+            "A3":Product("Water",15,0)
         }
-        self.state=ProductSelectionState()
-        self.balance=0
+        self.state:State=ProductSelectionState()
         self.selectedCode=None
-
-    def select(self,code):
-        self.state.select_product(self,code)
-    def insert(self,amount):
-        self.state.insert_money(self,amount)
+        self.balance=0
+        self._lock=threading.RLock()
+        self.txn_lock=threading.Lock()
+    def set_state(self,state):
+        with self._lock:
+            self.state=state
+    def select(self,product):
+        self.txn_lock.acquire()
+        try:
+            with self._lock:
+                self.state.select_product(self,product)
+        except Exception:
+            self.txn_lock.release()
+            raise
+    def insert(self,amount:Denomination):
+        with self._lock:
+            self.state.insert_money(self,amount)
     def process(self):
-        self.state.process_signal(self)
+        with self._lock:
+            self.state.process_signal(self)
     def dispense(self):
-        self.state.dispense(self)
+        with self._lock:
+            self.state.dispense(self) 
     def deliver(self):
-        self.state.deliver(self)
-    def setState(self,nextState):
-        self.state=nextState
+        with self._lock:
+            self.state.deliver(self)
+        self.txn_lock.release()
     def reset_machine(self):
-        self.balance=0
-        self.selectedCode=None
+        with self._lock:
+            self.balance=0
+            self.selectedCode=None
 
 class ProductSelectionState(State):
-    def select_product(self,machine,code):
-        product=machine.inventory.get(code)
+    def select_product(self, machine:VendingMachine, code:str):
+        product=machine._inventory.get(code)
         if(product is None):
-            print("Invalid product code")
-            return 
+            raise ValueError("Invalid product code")
         if(product.quantity<=0):
-            print("Out of stock")
-            return 
+            raise Exception("Out of stock")
         machine.selectedCode=code
-        print(product.name,'is selected')
-        machine.setState(PaymentValidationState())
+        machine.set_state(PaymentValidationState())
+        print("Product Selected")
 
-class PaymentValidationState(ABC):
-    def insert_money(self,machine,amount):
-        product=machine.inventory.get(machine.selectedCode)
-        machine.balance+=amount
-        if(machine.balance<product.price):
-            print('insufficient funds')
-            return 
-        print('payment processed')
-        machine.setState(ProcessSignalState())
+class PaymentValidationState(State):
+    def insert_money(self, machine:VendingMachine, amount:Denomination):
+        product=machine._inventory.get(machine.selectedCode)
+        change=0
+        machine.balance+=amount.value
+        if(product.price>machine.balance):
+            print("Insufficient funds")
+            return
+        if(machine.balance>product.price):
+            change=machine.balance-product.price
+        if(change>0):
+            print(f"Change: ${change} is returned")
+        print("Payment Validated")
+        machine.set_state(ProcessSignalState())
 
-class ProcessSignalState(ABC):
-    def process_signal(self,machine):
-        print('sending signal')
-        print('signal sent')
-        machine.setState(DispenseState())
+class ProcessSignalState(State):
+    def process_signal(self, machine):
+        print("sending signal")
+        print("signal sent")
+        machine.set_state(DispenseState())
 
-class DispenseState(ABC):
-    def dispense(self,machine):
-        product=machine.inventory[machine.selectedCode]
+class DispenseState(State):
+    def dispense(self, machine:VendingMachine):
+        product=machine._inventory.get(machine.selectedCode)
         product.quantity-=1
-        print('dispensing product...')
-        machine.setState(DeliverState())
+        print("Dispensing product")
+        machine.set_state(DeliverState())
 
-class DeliverState(ABC):
-    def deliver(self,machine):
-        print('product is delivered')
+class DeliverState(State):
+    def deliver(self, machine):
+        print("Product is delivered!!")
         machine.reset_machine()
-        machine.setState(ProductSelectionState())
-
-
-vm=VendingMachine()
-vm.select('A2')
-vm.insert(20)
-vm.process()
-vm.dispense()
-vm.deliver()
+        machine.set_state(ProductSelectionState())
+    
+            
+if __name__ == "__main__":
+    vm = VendingMachine()
+    vm.select("A1")
+    vm.insert(Denomination.COIN_10)
+    vm.insert(Denomination.COIN_10)
+    vm.insert(Denomination.COIN_10)   # balance hits 30, price 25 -> change $5
+    vm.process()
+    vm.dispense()
+    vm.deliver()
+        
